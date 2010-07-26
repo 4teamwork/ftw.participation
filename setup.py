145320140b4453f8852cd3cff3098a7ba581f6b0
Jonas Baumann <jone@jone.ch>
2010-07-26 17:11:57 +0200

    added maintainer

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import setup, find_packages
 import os
 
 version = open('ftw/participation/version.txt').read().strip()
+maintainer = 'Jonas Baumann'
 
 setup(name='ftw.participation',
       version=version,
-      description="Provide participation functionality for workspaces",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="Provide participation functionality for workspaces" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         ],
       keywords='ftw participation',
-      author='%s, 4teamwork GmbH',
+      author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       url='http://psc.4teamwork.ch/4teamwork/ftw/ftw-participation',
       license='GPL2',
@@ -22,10 +24,12 @@ setup(name='ftw.participation',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
