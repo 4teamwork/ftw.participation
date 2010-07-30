6e65e14aad2e6d496b83acb8af93c2f1244f07f2
Jonas Baumann <jone@jone.ch>
2010-07-30 16:39:05 +0200

    added accept invitations view

---
+++ ftw/participation/browser/accept.py
@@ -0,0 +1,153 @@
+from Products.Five.browser import BrowserView
+from zope.i18n import translate
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from Products.statusmessages.interfaces import IStatusMessage
+from ftw.participation import _
+from Products.CMFCore.utils import getToolByName
+from ftw.participation.interfaces import IInvitationStorage
+from ftw.participation.interfaces import IParticipationSetter
+from zope.component import getMultiAdapter, getUtility
+from Products.CMFCore.interfaces import IPropertiesTool
+
+
+
+class AcceptInvitation(BrowserView):
+    """Accept a invitation. This either called directly from the browser (with
+    a url like .../@@accept_invitations?iid=XXX) or through the invitations
+    view (with unrestrictedTravers('@@accept_invitations')(iid) ).
+
+    """
+
+    def __call__(self, iid=None):
+        """
+        """
+        if not iid:
+            iid = self.request.get('iid')
+        # globals
+        self.storage = IInvitationStorage(self.context)
+        self.invitation = self.storage.get_invitation_by_iid(iid)
+        self.target = self.invitation.get_target()
+
+        # sanity check
+        mtool = getToolByName(self.context, 'portal_membership')
+        self.member = mtool.getAuthenticatedMember()
+        valid_emails = (self.member.getId(),
+                        self.member.getProperty('email', object()))
+        if not self.invitation or self.invitation.email not in valid_emails:
+            msg = _(u'error_invitation_not_found',
+                    default=u'Could not find the invitation.')
+            IStatusMessage(self.request).addStatusMessage(msg, type='error')
+            return self.request.RESPONSE.redirect(self.context.portal_url())
+
+        self.set_participation()
+        self.send_email()
+
+        # add status message
+        msg = _(u'info_invitation_accepted',
+                default=u'You have accepted the invitation.')
+        IStatusMessage(self.request).addStatusMessage(msg, type='info')
+
+        # destroy the invitation
+        self.storage.remove_invitation(self.invitation)
+        del self.invitation
+
+        # redirect context (where the user now has access)
+        return self.request.RESPONSE.redirect(self.target.absolute_url())
+
+
+    def set_participation(self):
+        """Sets up participation up.
+        """
+        # participate the user (using the adapter)
+        setter = getMultiAdapter((self.target, self.request,
+                                  self.invitation, self.member),
+                                 IParticipationSetter)
+        setter()
+
+    def send_email(self):
+        """Sends the notification email to the invitor.
+        """
+        properties = getUtility(IPropertiesTool)
+        mtool = getToolByName(self.context, 'portal_membership')
+        # prepare from address for header
+        header_from = Header(properties.email_from_name.decode('utf-8'),
+                             'iso-8859-1')
+        header_from.append(u'<%s>' % properties.email_from_address.
+                           decode('utf-8'),
+                           'iso-8859-1')
+
+        # To
+        to_member = mtool.getMemberById(self.invitation.inviter)
+        if not to_member.getProperty('email'):
+            msg = _(u'error_inviter_email_missing',
+                    default=u'Invitor could not be notified: he has '
+                    'no email address defined.')
+            IStatusMessage(self.request).addStatusMessage(msg, type='error')
+            return
+        header_to = Header(to_member.getProperty('fullname',
+                                                 to_member.getId()).decode('utf8'),
+                           'iso-8859-1')
+        header_to.append(u'<%s>' % to_member.getProperty('email'))
+
+        # Subject
+        header_subject = Header(unicode(self.get_subject()), 'iso-8859-1')
+
+        # Options for template
+        options = {
+            'invitation': self.invitation,
+            'inviter': to_member,
+            'invited': self.member,
+            'url': self.context.absolute_url(),
+            }
+
+        # make the mail
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = header_subject
+        msg['From'] = header_from
+        msg['To'] = header_to
+
+        # get the body views
+        html_view = self.get_mail_body_html_view()
+        text_view = self.get_mail_body_text_view()
+
+        # render and emmbed body
+        text_body = text_view(**options).encode('utf-8')
+        msg.attach(MIMEText(text_body, 'plain', 'utf-8'))
+        html_body = html_view(**options).encode('utf-8')
+        msg.attach(MIMEText(html_body), 'html', 'utf-8')
+
+        # send the email
+        mh = getToolByName(self.context, 'MailHost')
+        mh.send(msg, to=to_member.getProperty('email'))
+
+    def get_mail_body_html_view(self):
+        return self.context.unrestrictedTraverse('@@invitation_accepted_mail_html')
+
+    def get_mail_body_text_view(self):
+        return self.context.unrestrictedTraverse('@@invitation_accepted_mail_text')
+
+    def get_subject(self):
+        """Returns the translated subject of the email.
+        """
+        member = getToolByName(self.context,
+                               'portal_membership').getAuthenticatedMember()
+        fullname = member.getProperty('fullname', member.getId())
+        context_title = self.context.pretty_title_or_id().decode('utf-8')
+        # -- i18ndude hint --
+        if 0:
+            _(u'mail_invitation_subject',
+              default=u'The Invitation to participate in ${title} ' +\
+                  u'was accepted by ${user}',
+              mapping=dict(title=context_title,
+                           user=fullname))
+        # / -- i18ndude hint --
+        # do not use translation messages - translate directly
+        return translate(u'mail_invitation_accepted_subject',
+                         domain='ftw.participation',
+                         context=self.request,
+                         default=u'The Invitation to participate in ${title} ' +\
+                             u'was accepted by ${user}',
+                         mapping=dict(title=context_title,
+                                      user=fullname))
---
+++ ftw/participation/setter.py
@@ -0,0 +1,70 @@
+from ftw.participation import interfaces
+from zope.component import adapts
+from zope.interface import Interface
+from zope.interface import implements
+import AccessControl
+
+
+class DefaultParticipationSetter(object):
+    """Sets up a participation on a object for a user.
+    Discriminators:
+    - context
+    - request
+    - invitation
+    - member
+
+    """
+
+    adapts(interfaces.IParticipationSupport,
+           interfaces.IParticipationBrowserLayer,
+           interfaces.IInvitation,
+           Interface)
+
+    implements(interfaces.IParticipationSetter)
+
+    def __init__(self, context, request, invitation, member):
+        self.context = context
+        self.request = request
+        self.invitation = invitation
+        self.member = member
+
+    def __call__(self):
+        """Participates the user
+        """
+        return self.participate_user()
+
+    def participate_user(self):
+        """Participates `self.user` on `self.context`.
+        """
+        local_roles = dict(self.context.get_local_roles())
+        # get all current local roles of the user on this context
+        user_roles = list(local_roles.get(self.member.getId(), []))
+        user_roles.extend(self.roles())
+        # make the roles unique
+        user_roles = dict(zip(user_roles, user_roles)).keys()
+
+        # Set the local roles with the security of the inviter. If
+        # he has no longer permissions on this context this will
+        # fail.
+        _old_security_manager = AccessControl.getSecurityManager()
+        _new_user = self.context.acl_users.getUserById(
+            self.invitation.inviter)
+        AccessControl.SecurityManagement.newSecurityManager(
+            self.request, _new_user)
+        try:
+            self.context.manage_setLocalRoles(self.member.getId(),
+                                              user_roles)
+            self.context.reindexObjectSecurity()
+        except:
+            AccessControl.SecurityManagement.setSecurityManager(
+                _old_security_manager)
+            raise
+        else:
+            AccessControl.SecurityManagement.setSecurityManager(
+                _old_security_manager)
+
+    def roles(self):
+        """List of roles to give the `self.user` on the `self.context`.
+        """
+        return ('Reader',)
+        
