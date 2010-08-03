13b770495aad8bd14806fd8c6e3e2f36e2e808a2
Jonas Baumann <jone@jone.ch>
2010-08-03 17:23:00 +0200

    added script for selecting all invitations

---
+++ ftw/participation/browser/resources/select_all.js
@@ -0,0 +1,14 @@
+/* invitations view select all */
+jQuery(function($) {
+     $('.select_all_invitations').click(
+       function() {
+         var parents = $(this).parents('table:first').find('input');
+         if ($(this).attr('checked') == false) {
+           parents.attr('checked', null);
+         }
+         else {
+           parents.attr('checked', 'checked');
+         }
+       });
+   });
+
