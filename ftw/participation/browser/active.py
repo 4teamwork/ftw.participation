3ecfbfaeef3790be14239ed571195c7bfc7bda6c
Jonas Baumann <jone@jone.ch>
2010-08-03 17:57:18 +0200

    added condition for invitation action

---
+++ ftw/participation/browser/active.py
@@ -0,0 +1,11 @@
+from Products.Five.browser import BrowserView
+from ftw.participation.interfaces import IParticipationSupport
+
+
+class ParticipationActive(BrowserView):
+    """Checks if the participation is active on this context.
+    
+    """
+
+    def __call__(self):
+        return IParticipationSupport.providedBy(self.context)
