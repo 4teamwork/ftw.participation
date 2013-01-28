35da70f8d1ff77ace902308cccb1c013acf38bd4
Jonas Baumann <jone@jone.ch>
2013-01-28 11:14:02 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -21,15 +21,13 @@ setup(name='ftw.participation',
 
 
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.0',
         'Framework :: Plone :: 4.1',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Framework :: Plone :: 4.2',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
