799ae290f0835c052e35d2f0c1aa90ae0b221905
Jonas Baumann <jone@jone.ch>
2016-11-29 23:10:26 +0100

    Drop Plone 4.2 support.
    The reason for dropping Plone 4.2 support is that the new
    optional dependency ftw.activity (tests / exras) does not support Plone
    4.2 and therefore tests are failing.

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(name='ftw.participation',
 
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.2',
         'Framework :: Plone :: 4.3',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
