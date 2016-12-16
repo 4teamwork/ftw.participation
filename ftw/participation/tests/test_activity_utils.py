3e00a4da6b6b2587060a1eb13ab2f9dd8a62698a
Jonas Baumann <jone@jone.ch>
2016-12-16 11:21:23 +0100

    Improve ftw.lawgiver support for translated role titles.
    The standard way to get the role title for translation is to use an
    ISharingPageRole utility, which has the global title of this role.
    
    ftw.lawgiver introduces a concept of workflow specific titles /
    translations for roles, which are hooked into the ISharingPageRole
    utility by looking up the traversed context from the request.
    
    This concept does not work though when rendering roles for multiple
    context: in this case for a list of activities, which may have contexts
    with different workflows and thus different role titles.
    
    In order to address this, we now directly lookup the role title from the
    the context bound IDynamicRoleAdapter (introduced by ftw.lawgiver), so
    that we do not depend on the traversed context from the request.

--- ftw/participation/tests/test_activity_utils.py
+++ ftw/participation/tests/test_activity_utils.py
@@ -7,20 +7,20 @@ class TestInvitationRenderer(FunctionalTestCase):
     def test_translate_and_join_roles__1_role(self):
         self.assertEquals(
             'Can add',
-            translate_and_join_roles(('Contributor',), None))
+            translate_and_join_roles(('Contributor',), None, None))
 
     def test_translate_and_join_roles__2_roles(self):
         self.assertEquals(
             'Can add and Can view',
-            translate_and_join_roles(('Reader', 'Contributor'), None))
+            translate_and_join_roles(('Reader', 'Contributor'), None, None))
 
     def test_translate_and_join_roles__3_roles(self):
         self.assertEquals(
             'Can add, Can edit and Can view',
-            translate_and_join_roles(('Reader', 'Editor', 'Contributor'), None))
+            translate_and_join_roles(('Reader', 'Editor', 'Contributor'), None, None))
 
     def test_translate_and_join_roles__4_roles(self):
         self.assertEquals(
             'Can add, Can edit and Can view',
             translate_and_join_roles(
-                ('Reader', 'Editor', 'Contributor', 'Manager'), None))
+                ('Reader', 'Editor', 'Contributor', 'Manager'), None, None))
