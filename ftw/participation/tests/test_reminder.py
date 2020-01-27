a8da3f7559dab469b91c3f0cb4e149d839ed23c6
2e12 <gabriel.beutler@4teamwork.ch>
2020-01-27 08:15:14 +0100

    Add test for reminder resend button.
    In order to test if the resend button does work.

--- ftw/participation/tests/test_reminder.py
+++ ftw/participation/tests/test_reminder.py
@@ -1,13 +1,14 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from ftw.participation.interfaces import IInvitationStorage
-from ftw.participation.interfaces import IParticipationRegistry
 from ftw.participation.interfaces import IParticipationSupport
 from ftw.participation.tests.layer import FTW_PARTICIPATION_FUNCTIONAL_TESTING
 from ftw.testbrowser import browsing
 from ftw.testing.mailing import Mailing
+from plone.app.testing import login
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+from plone import api
 from unittest import TestCase
-from zope.interface import alsoProvides
 import transaction
 
 
@@ -16,10 +17,20 @@ class TestReminder(TestCase):
     layer = FTW_PARTICIPATION_FUNCTIONAL_TESTING
 
     def setUp(self):
-        self.folder = create(Builder('folder').titled('Folder'))
-        alsoProvides(self.folder, IParticipationSupport)
+
         self.portal = self.layer['portal']
         Mailing(self.portal).set_up()
+        transaction.commit()
+
+        self.folder = create(Builder('folder')
+                             .titled(u'The F\xf6lder')
+                             .providing(IParticipationSupport))
+
+        self.inviter = api.user.get(userid=TEST_USER_ID)
+        self.grant('Manager')
+
+    def grant(self, *roles):
+        setRoles(self.portal, TEST_USER_ID, list(roles))
         transaction.commit()
 
     def tearDown(self):
@@ -27,18 +38,33 @@ class TestReminder(TestCase):
 
     @browsing
     def test_send_reminder_mail(self, browser):
-        hugo = create(Builder('user').named('Hugo', 'Boss'))
-        matthias = create(Builder('user')
-                         .named('Matthias', 'Osswald'))
+
+        hugo = create(Builder('user')
+                          .named(u'Hugo', u'B\xf6ss'))
 
         create(Builder('invitation')
-               .inviting(matthias)
+               .inviting(hugo)
                .to(self.folder)
-               .invited_by(hugo))
+               .invited_by(self.inviter))
 
-        browser.login(self.hugo.getId()).visit(self.folder,
-                                               view='participants')
-        browser.css('td input').first.click()
+        browser.login()
+        browser.visit(self.folder, view='participants')
+        browser.fill({
+            'invitations:list': True
+        })
         browser.find('Resend Invitations').click()
 
-        mail = Mailing(portal).pop()
+        mail = Mailing(self.portal).pop()
+        self.assertIn("hugo@boss.com", mail)
+        self.assertIn("This is a reminder.", mail)
+
+    @browsing
+    def test_no_user_selected(self, browser):
+
+        browser.login()
+        browser.visit(self.folder, view='participants')
+
+        browser.find('Resend Invitations').click()
+
+        mails = Mailing(self.portal)
+        self.assertEqual(len(mails.get_messages()), 0)
